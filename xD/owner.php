f2a53402c6480ec910f5d65d388b1ead4d2b999c
Amelhmg <46347807+Amelhmg@users.noreply.github.com>
2019-02-23 15:26:38 +0100

    Add files via upload

---
+++ xD/owner.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Travel Agency</title>
+    <script type="text/javascript">
+      function Soumettre(form2){
+        alert("Vous receverez un E-mail de confirmation à ");
+      }
+    </script>
+</head>
+<body>
+    <div class="maindiv">
+        <header class="header">
+                    <a class="logo" href="index.php"> > Travel Agency < </a>
+        </header>
+
+   
+<div class="containers">
+  <h1>Formulaire de contact</h1>
+  <form name="xxxxxxxxxxxxxxxxxxxxx">
+    <label for="fname">Nom & prénom</label>
+    <input type="text" id="fname" name="firstname" placeholder="Votre nom et prénom">
+
+    <label for="sujet">Sujet</label>
+    <input type="text" id="sujet" name="sujet" placeholder="L'objet de votre message">
+
+    <label for="emailAddress">Email</label>
+    <input id="emailAddress" type="email" name="email" placeholder="Votre email">
+
+
+    <label for="subject">Message</label>
+    <textarea id="subject" name="subject" placeholder="Votre message" style="height:200px"></textarea>
+
+    <input type="submit" value="Envoyer">
+  </form>
+</div>
+
+
+
+
+
+
+      
+        <footer class="footer">
+            <div class="contactinformation">
+                    <h4 class="h4contactinformation">Contact Information</h4>
+                    <p class="info">Contact : contact@travelagency.com</p>
+                    <p class="info">Tel : +21300000000</p>
+                    <p class="info">Fax : +21300000000</p>
+            </div>
+
+            <div class="copyrights">
+                <p>Travel Agency inc. Tout droit réservé</p>
+            </div>
+            <div class="follow">
+                <h4 class="followus">Nous suivre</h4>
+                <a class="followlinks" href="https://www.facebook.com/" target="_blank">Facebook</a>
+                <a class="followlinks" href="https://www.instagram.com/" target="_blank">Instagram</a>
+                <a class="followlinks" href="https://www.twitter.com/" target="_blank">Twitter</a>
+            </div>
+        </footer>
+    </div>
+</body>
+</html>

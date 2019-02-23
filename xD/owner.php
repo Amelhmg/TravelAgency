2ec873b811a22b4e4b87432c374673982de7f0ba
Amelhmg <46347807+Amelhmg@users.noreply.github.com>
2019-02-23 15:39:14 +0100

    Update owner.php

--- xD/owner.php
+++ xD/owner.php
@@ -25,15 +25,15 @@
     <label for="fname">Nom & prénom</label>
     <input type="text" id="fname" name="firstname" placeholder="Votre nom et prénom">
 
-    <label for="sujet">Sujet</label>
-    <input type="text" id="sujet" name="sujet" placeholder="L'objet de votre message">
+    <label for="sujet">Hotel</label>
+    <input type="text" id="sujet" name="sujet" placeholder="Le nom de votre hotel ">
 
     <label for="emailAddress">Email</label>
     <input id="emailAddress" type="email" name="email" placeholder="Votre email">
 
 
-    <label for="subject">Message</label>
-    <textarea id="subject" name="subject" placeholder="Votre message" style="height:200px"></textarea>
+    <label for="subject">Description</label>
+    <textarea id="subject" name="subject" placeholder="Veuillez descrire votre hotel " style="height:200px"></textarea>
 
     <input type="submit" value="Envoyer">
   </form>
